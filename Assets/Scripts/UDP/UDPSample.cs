a12fc0579627a8ec63b7b1b3db824484006acdbe
Finney <finneytang@gmail.com>
2019-01-14 13:01:50 +0800

    add udp

---
+++ Assets/Scripts/UDP/UDPSample.cs
@@ -0,0 +1,43 @@
+﻿using Network.UDP;
+using System.Text;
+using UnityEngine;
+
+public class UDPSample : MonoBehaviour
+{
+    private UDPListener m_ServerSession = new UDPListener();
+    private UDPUser m_ClientSession = new UDPUser();
+    void Start ()
+    {
+        if (m_ServerSession.Init("127.0.0.1", 30000))
+        {
+            m_ServerSession.SetDataHandler((data, addr) =>
+            {
+                Debug.Log("<color=green>Hello, User from " + addr.ToString() + "</color>");
+            });
+            m_ServerSession.Start();
+        }
+        if (m_ClientSession.Init("127.0.0.1", 30000))
+        {
+            m_ClientSession.SetEchoHandler(delegate ()
+            {
+                string msg = "Hello, Server";
+                Debug.Log("<color=yellow>" + msg + "</color>");
+                return Encoding.ASCII.GetBytes(msg);
+            });
+            m_ClientSession.Start();
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        if(m_ClientSession != null)
+        {
+            m_ClientSession.Close();
+            m_ClientSession = null;
+        }
+        if (m_ClientSession != null)
+        {
+            m_ClientSession.Close();
+            m_ClientSession = null;
+        }
+    }
+}
