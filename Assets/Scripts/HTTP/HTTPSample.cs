d3dcd501ac1f2824d17b69b8d2fc8b96e1fe74f7
Finney <finneytang@gmail.com>
2019-01-15 13:58:45 +0800

    add http sample

---
+++ Assets/Scripts/HTTP/HTTPSample.cs
@@ -0,0 +1,47 @@
+﻿using Network.Core;
+using Network.HTTP;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class HTTPSample : MonoBehaviour
+{
+    private List<HTTPRequest> m_PendingRequests = new List<HTTPRequest>();
+    private void Start()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            TestGet(i);
+        }
+        TestPost();
+    }
+    private void Update()
+    {
+        for(int i = m_PendingRequests.Count - 1; i >= 0; i--)
+        {
+            m_PendingRequests[i].CheckPendingRequest();
+        }
+    }
+    private void TestGet(int index)
+    {
+        var request = new HTTPRequest();
+        request.Get("http://httpbin.org/", "get", (result, data) =>
+        {
+            if (result == true)
+            {
+                ColoredLogger.Log(Encoding.ASCII.GetString(data), ColoredLogger.LogColor.Green);
+            }
+            else
+            {
+                ColoredLogger.Log("Request failed: " + request.LastError, ColoredLogger.LogColor.Red);
+            }
+            m_PendingRequests.Remove(request);
+        }, "name", "Reqeust" + index, "index", index);
+        //add to pending list
+        m_PendingRequests.Add(request);
+    }
+    private void TestPost()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Network/HTTPRequest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Network.HTTP
+{
+    class HTTPRequest
+    {
+        private UnityWebRequest m_Request;
+        private UnityWebRequestAsyncOperation m_AsyncOp;
+        private Action<bool, byte[]> m_RequestFinishedAction;
+        private string m_LastError = string.Empty;
+        public string LastError
+        {
+            get
+            {
+                return m_LastError;
+            }
+        }
+        public void Get(string addr, string cmd, Action<bool, byte[]> onRequestFinished, params object[] args)
+        {
+            if (args.Length % 2 != 0)
+            {
+                onRequestFinished.Invoke(false, null);
+                return;
+            }
+            int argLen = args.Length / 2;
+            string paramStr = "";
+            for (int i = 0; i < argLen; i++)
+            {
+                if (i == 0)
+                {
+                    paramStr += "?";
+                }
+                else
+                {
+                    paramStr += "&";
+                }
+                paramStr += (args[i * 2].ToString() + "=" + Uri.EscapeDataString(args[i * 2 + 1].ToString()));
+            }
+            string url = Uri.EscapeUriString(addr) + cmd + paramStr;
+            m_Request = UnityWebRequest.Get(url);
+            m_RequestFinishedAction = onRequestFinished;
+            m_AsyncOp = m_Request.SendWebRequest();
+        }
+        public void CheckPendingRequest()
+        {
+            if(m_AsyncOp == null)
+            {
+                return;
+            }
+            if(m_AsyncOp.isDone)
+            {
+                bool hasError = m_Request.isNetworkError || m_Request.isHttpError;
+                m_LastError = m_Request.error;
+                m_RequestFinishedAction.Invoke(!hasError, m_Request.downloadHandler.data);
+                Dispose();
+            }
+        }
+        private void Dispose()
+        {
+            m_Request.Dispose();
+            m_RequestFinishedAction = null;
+            m_AsyncOp = null;
+        }
+    }
+}
