a72e3019ada25840cbe44f80b06380d8e3a0f5b9
Finney <finneytang@gmail.com>
2019-04-26 01:01:18 +0800

    add sync input sample

---
+++ Assets/Scripts/SyncInput/SyncInputSampleClient.cs
@@ -0,0 +1,122 @@
+﻿using Network.UDP;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SyncInputSampleClient : MonoBehaviour
+{
+    public Transform SimObjectTF;
+    public Transform PreObjectTF;
+
+    private UDPClient m_ClientSession = new UDPClient();
+    private Queue<byte[]> m_ClientRecvedData = new Queue<byte[]>();
+    void Start()
+    {
+        if (m_ClientSession.Init("127.0.0.1", 30000))
+        {
+            m_ClientSession.Start();
+        }
+    }
+    void Update()
+    {
+        ClientSimUpdate();
+        ClientPresentationUpdate();
+    }
+    private bool m_HasClientConnected = false;
+    private Vector3 m_TargetPos = Vector3.zero;
+    private Quaternion m_TargetRot = Quaternion.identity;
+    private Vector3 m_StartPos;
+    private Quaternion m_StartRot;
+    private float m_SimTime;
+    private float m_TotalTime;
+    void ClientSimUpdate()
+    {
+        if (m_HasClientConnected)
+        {
+            SendInput();
+        }
+        if (m_ClientSession.GetRecvedData(m_ClientRecvedData))
+        {
+            int frameCount = 0;
+            while (m_ClientRecvedData.Count != 0)
+            {
+                InputMsg msg = new InputMsg();
+                msg.Unserialize(m_ClientRecvedData.Dequeue());
+                UpdateByServer(msg);
+                frameCount++;
+            }
+            SimObjectTF.position = m_SimPos;
+            SimObjectTF.rotation = m_SimRot;
+
+            m_TargetPos = m_SimPos;
+            m_TargetRot = m_SimRot;
+            m_StartPos = PreObjectTF.position;
+            m_StartRot = PreObjectTF.rotation;
+            m_TotalTime = Time.fixedDeltaTime * frameCount;
+            m_SimTime = 0;
+        }
+    }
+    public void ClientPresentationUpdate()
+    {
+        if (m_TotalTime < Mathf.Epsilon)
+        {
+            return;
+        }
+        m_SimTime += Time.deltaTime;
+        float ratio = Mathf.Clamp01(m_SimTime / m_TotalTime);
+        PreObjectTF.position = Vector3.Lerp(m_StartPos, m_TargetPos, ratio);
+        PreObjectTF.rotation = Quaternion.Slerp(m_StartRot, m_TargetRot, ratio);
+    }
+    void SendInput()
+    {
+        InputMsg msg = new InputMsg();
+        if (Input.GetKey(KeyCode.A))
+        {
+            msg.Dir.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            msg.Dir.x = 1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            msg.Dir.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            msg.Dir.y = -1;
+        }
+        m_ClientSession.Send(msg.Serialize());
+    }
+    private Vector3 m_SimPos = Vector3.zero;
+    private Quaternion m_SimRot = Quaternion.identity;
+    void UpdateByServer(InputMsg msg)
+    {
+        Vector3 dir = new Vector3(msg.Dir.x, 0, msg.Dir.y);
+        dir = Camera.main.transform.TransformDirection(dir);
+        dir.y = 0;
+        dir.Normalize();
+        m_SimPos += 5 * Time.fixedDeltaTime * dir;
+        if(dir.sqrMagnitude > Mathf.Epsilon)
+        {
+            m_SimRot = Quaternion.LookRotation(dir);
+        }
+    }
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 50), "Connect"))
+        {
+            m_ClientSession.Send(new byte[] { 0 });
+            m_HasClientConnected = true;
+        }
+    }
+    void OnApplicationQuit()
+    {
+        if (m_ClientSession != null)
+        {
+            m_ClientSession.Close();
+            m_ClientSession = null;
+        }
+    }
+}
---
+++ Assets/Scripts/SyncInput/SyncInputSampleServer.cs
@@ -0,0 +1,75 @@
+﻿using Network.UDP;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncInputSampleServer : MonoBehaviour
+{
+    private UDPListener m_ServerSession = new UDPListener();
+    private Queue<byte[]> m_ServerRecvedData = new Queue<byte[]>();
+    void Start()
+    {
+        Time.fixedDeltaTime = 0.1f;
+
+        if (m_ServerSession.Init("127.0.0.1", 30000))
+        {
+            m_ServerSession.Start();
+        }
+    }
+    void Update()
+    {
+        ServerUpdate();
+    }
+    void FixedUpdate()
+    {
+        ServerFixedUpdate();
+    }
+    private bool m_HasClientConnected = false;
+    private int m_CurrentFrame = 0;
+    private InputMsg m_ClientInput = new InputMsg();
+    void ServerUpdate()
+    {
+        if (m_HasClientConnected)
+        {
+            if (m_ServerSession.GetRecvedData(m_ServerRecvedData))
+            {
+                while (m_ServerRecvedData.Count != 0)
+                {
+                    m_ClientInput.Unserialize(m_ServerRecvedData.Dequeue());
+                }
+            }
+        }
+    }
+    void ServerFixedUpdate()
+    {
+        if (m_HasClientConnected == false)
+        {
+            if (m_ServerSession.GetRecvedData(m_ServerRecvedData))
+            {
+                while (m_ServerRecvedData.Count != 0)
+                {
+                    m_HasClientConnected = true;
+                    m_ServerRecvedData.Dequeue();
+                }
+            }
+        }
+        else
+        {
+            m_CurrentFrame++;
+            m_ServerSession.Send(m_ClientInput.Serialize());
+        }
+    }
+    private void OnGUI()
+    {
+        int margin = (int)(Mathf.Min(Screen.width, Screen.height) * 0.25f);
+        GUI.Label(new Rect(margin, margin, Screen.width - 2 * margin, 20), m_CurrentFrame.ToString());
+    }
+    //----------------------------------------------------------
+    void OnApplicationQuit()
+    {
+        if (m_ServerSession != null)
+        {
+            m_ServerSession.Close();
+            m_ServerSession = null;
+        }
+    }
+}
