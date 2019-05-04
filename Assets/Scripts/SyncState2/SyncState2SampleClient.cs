3b5ddacb2298c099cc6ccdf833f570ec6f83ddfd
Finney <finneytang@gmail.com>
2019-05-04 22:24:23 +0800

    add sync state 2

---
+++ Assets/Scripts/SyncState2/SyncState2SampleClient.cs
@@ -0,0 +1,83 @@
+﻿using Network.UDP;
+using UnityEngine;
+
+public class SyncState2SampleClient : MonoBehaviour
+{
+    public Transform SimObjectTF;
+
+    private UDPClient m_ClientSession = new UDPClient();
+    void Start()
+    {
+        if (m_ClientSession.Init("127.0.0.1", 30000))
+        {
+            m_ClientSession.Start();
+        }
+    }
+    void Update()
+    {
+        ClientSimUpdate();
+    }
+    private bool m_HasClientConnected = false;
+    private float m_NextUploadTime;
+    void ClientSimUpdate()
+    {
+        if (m_HasClientConnected)
+        {
+            //update simulation
+            Vector3 dir = Vector3.zero;
+            if (Input.GetKey(KeyCode.A))
+            {
+                dir.x = -1;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                dir.x = 1;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                dir.z = 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                dir.z = -1;
+            }
+            dir = Camera.main.transform.TransformDirection(dir);
+            dir.y = 0;
+            dir.Normalize();
+            Vector3 pos = SimObjectTF.position;
+            pos += 6 * Time.deltaTime * dir;
+            SimObjectTF.position = pos;
+            if (dir.sqrMagnitude > Mathf.Epsilon)
+            {
+                SimObjectTF.rotation = Quaternion.LookRotation(dir);
+            }
+            //send state msg
+            if (Time.time > m_NextUploadTime)
+            {
+                UploadStateMsg msg = new UploadStateMsg();
+                msg.TargetPosition = SimObjectTF.position;
+                msg.TargetOrientation = SimObjectTF.rotation;
+                msg.TimeStamp = Time.time;
+
+                m_ClientSession.Send(msg.Serialize());
+                m_NextUploadTime = Time.time + 0.1f;
+            }
+        }
+    }
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 50), "Connect"))
+        {
+            m_ClientSession.Send(new byte[] { 0 });
+            m_HasClientConnected = true;
+        }
+    }
+    void OnApplicationQuit()
+    {
+        if (m_ClientSession != null)
+        {
+            m_ClientSession.Close();
+            m_ClientSession = null;
+        }
+    }
+}
---
+++ Assets/Scripts/SyncState2/SyncState2SampleServer.cs
@@ -0,0 +1,60 @@
+﻿using Network.UDP;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncState2SampleServer : MonoBehaviour
+{
+    public Transform ServerObjectTF;
+
+    private UDPListener m_ServerSession = new UDPListener();
+    private Queue<byte[]> m_ServerRecvedData = new Queue<byte[]>();
+
+    void Start()
+    {
+        if (m_ServerSession.Init("127.0.0.1", 30000))
+        {
+            m_ServerSession.Start();
+        }
+    }
+    void Update()
+    {
+        ServerUpdate();
+    }
+    private bool m_HasClientConnected = false;
+    private float m_LastTimeStamp;
+    void ServerUpdate()
+    {
+        if (m_ServerSession.GetRecvedData(m_ServerRecvedData))
+        {
+            while (m_ServerRecvedData.Count != 0)
+            {
+                var data = m_ServerRecvedData.Dequeue();
+                if (m_HasClientConnected == false)
+                {
+                    m_HasClientConnected = true;
+                }
+                else
+                {
+                    UploadStateMsg msg = new UploadStateMsg();
+                    msg.Unserialize(data);
+                    if (msg.TimeStamp > m_LastTimeStamp)
+                    {
+                        ServerObjectTF.position = msg.TargetPosition;
+                        ServerObjectTF.rotation = msg.TargetOrientation;
+
+                        m_LastTimeStamp = msg.TimeStamp;
+                    }
+                }
+            }
+        }
+    }
+    //----------------------------------------------------------
+    void OnApplicationQuit()
+    {
+        if (m_ServerSession != null)
+        {
+            m_ServerSession.Close();
+            m_ServerSession = null;
+        }
+    }
+}
